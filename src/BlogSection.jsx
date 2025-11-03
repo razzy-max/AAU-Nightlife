29bb81092e576be07330d61922cbfa21a9a78726
Irabor Erasmus <eraskid6@gmail.com>
2025-11-03 13:46:55 +0100

    Fix syntax error in BlogSection.jsx import statement

--- src/BlogSection.jsx
+++ src/BlogSection.jsx
@@ -1,4 +1,4 @@
-oimport React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import './BlogSection.css';
